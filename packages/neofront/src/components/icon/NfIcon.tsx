271789c274d1ab3898efaa3be874778d32ef79ba
leosdad <pechansky@hypervisual.com.br>
2025-12-10 20:01:54 -0300

    Added '_blank' icon

--- packages/neofront/src/components/icon/NfIcon.tsx
+++ packages/neofront/src/components/icon/NfIcon.tsx
@@ -9,6 +9,7 @@ import { CSSProperties, JSX } from "react";
 import NfTablerIcon from "./NfTablerIcon";
 import NfMaterialIcon from "./NfMaterialIcon";
 import { useAppUI } from "context";
+import { Box } from "@mantine/core";
 
 // #endregion
 
@@ -28,9 +29,26 @@ export interface NfIconProps {
 // #region ------------------------------------------------------------------------------- Component
 
 export default function NfIcon(props: NfIconProps): JSX.Element {
+
+  const { icon, size, color, filled, style } = props;
+
+  if (icon == '_blank') {
+    return <Box
+      className={`material-icons-${filled ? '' : 'outlined'}`}
+      component="span"
+      style={{
+        fontSize: size,
+        color: color,
+        ...style,
+        width: size,
+        overflow: 'hidden',
+      }}
+    />;
+  }
+
   const { appCfg } = useAppUI();
 
-  switch(appCfg.theme.iconFamily) {
+  switch (appCfg.theme.iconFamily) {
     case 'material':
       return <NfMaterialIcon {...props} />;
     case 'tabler':
