dfff7690fc2c1421304b57f71a2a5a7f3547c166
leosdad <pechansky@hypervisual.com.br>
2025-12-23 19:44:01 -0300

    Enhanced pacientes view with numeric icons and form tabs in project SSAA

--- packages/app-ssaa/project/views/pacientes/loader.js
+++ packages/app-ssaa/project/views/pacientes/loader.js
@@ -10,16 +10,26 @@ import pacientesFields from "./fields.json";
 import instituicoes from "../instituicoes/data.json";
 import permissoes from "./data_permissoes.json";
 import status from "./data_status.json";
+import usuarios from "../usuarios/data.json";
+import projetos from "../projetos/data.json";
+import projetosFields from "../projetos/fields.json";
+
+// Acrescenta os registros-filhos à tabela principal
+pacientes.forEach(p => {
+  p.projetos = projetos.filter(pr => pr.paciente_id === p.id);
+});
 
 export default {
   listView,
   form,
   fieldConfig: {
-    pacientes: pacientesFields
+    pacientes: pacientesFields,
+    projetos: projetosFields,
   },
   data: {
     pacientes,
     status,
+    usuarios,
     instituicoes,
     permissoes
   },
