ec12e31ad59c4db89895d8bb9610663edc16d30b
leosdad <pechansky@hypervisual.com.br>
2025-12-23 18:55:21 -0300

    Enhanced usuarios view with numeric icons and form tabs in project SSAA

--- packages/app-ssaa/project/views/usuarios/loader.js
+++ packages/app-ssaa/project/views/usuarios/loader.js
@@ -7,15 +7,27 @@ import form from "./form.json";
 import usuarios from "./data.json";
 import usuariosFields from "./fields.json";
 
+import status from "./data_status.json";
 import instituicoes from "../instituicoes/data.json";
 import permissoes from "./data_permissoes.json";
-import status from "./data_status.json";
+import pacientes from "../pacientes/data.json";
+import pacientesFields from "../pacientes/fields.json";
+import projetos from "../projetos/data.json";
+import projetosFields from "../projetos/fields.json";
+
+// Acrescenta os registros-filhos à tabela principal
+usuarios.forEach(u => {
+  u.pacientes = pacientes.filter(p => p.usuario_id === u.id);
+  u.projetos = projetos.filter(i => i.usuario_id === u.id);
+});
 
 export default {
   listView,
   form,
   fieldConfig: {
-    usuarios: usuariosFields
+    usuarios: usuariosFields,
+    pacientes: pacientesFields,
+    projetos: projetosFields,
   },
   data: {
     usuarios,
